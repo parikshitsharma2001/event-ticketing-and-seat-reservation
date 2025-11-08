ef56cc8bcf1f2e5d31d17c66431e2f1c1275e97f
Himanshu Chugh <2024tm93601@wilp.bits-pilani.ac.in>
2025-11-07 22:23:30 -0800

    fixed compilation

--- seating-service/src/main/java/com/ticketing/seatingservice/dto/SeatAllocationRequest.java
+++ seating-service/src/main/java/com/ticketing/seatingservice/dto/SeatAllocationRequest.java
@@ -5,8 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
